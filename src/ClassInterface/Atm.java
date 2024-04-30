2cf5004ebc24f83a214078143d6a334df7798fca
sachinbasha <uchesachin96@gmail.com>
2024-04-30 15:44:51 +0530

    Merge branch 'sachinnavvenbranch' of https://github.com/Aggregation/Conflict.git into sachinnavvenbranch

--- src/ClassInterface/Atm.java
+++ src/ClassInterface/Atm.java
@@ -7,7 +7,7 @@ public interface Atm {
 	public void checkbalance();
 	public void withdraw();
 
-	public void checkbal();
+	public void checkba2();
 	public void sachin();
 	public void amar();
 	public void amar1();
